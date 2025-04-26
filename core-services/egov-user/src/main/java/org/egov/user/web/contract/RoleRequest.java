5f39acd006147753be487ec66fd37489f61cd923
mcd89 <mcdupyog@gmail.com>
2025-04-26 12:12:12 +0530

    Update RoleRequest.java
    Changes reverted

--- core-services/egov-user/src/main/java/org/egov/user/web/contract/RoleRequest.java
+++ core-services/egov-user/src/main/java/org/egov/user/web/contract/RoleRequest.java
@@ -4,11 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import org.egov.user.domain.model.Role;
 
-@NoArgsConstructor
+
 @Getter
 @Builder
 @AllArgsConstructor
