af3d690e55823492888dc0a6dc11495e6f90bc09
DevanshuJain-SSPL <devops@sparrowsoftech.net>
2025-04-28 10:59:09 +0530

    removed frontend

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/surveys/useSubmitSurveyResponse.js
+++
@@ -1,8 +0,0 @@
-import { Surveys } from "../../services/elements/Surveys";
-import { useMutation } from "react-query";
-
-const useSubmitSurveyResponse = (filters, config) => {
-    return useMutation((filters) => Surveys.submitSurveyResponse(filters));
-};
-
-export default useSubmitSurveyResponse;
