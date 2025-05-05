1514894fce17c8a0b6b3ff6155e5c3deffd2006b
umesh-sspl <umesh@sparrowsoftech.net>
2025-05-05 14:04:35 +0530

    revert hrms changes

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/employee/Login/login.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/employee/Login/login.js
@@ -61,33 +61,29 @@ const Login = ({ config: propsConfig, t, isDisabled }) => {
   }, [user]);
 
 
-const onLogin = async (data) => {
-  if (!data.city) {
-    alert("Please Select City!");
-    return;
-  }
+  const onLogin = async (data) => {
+    if (!data.city) {
+      alert("Please Select City!");
+      return;
+    }
+    setDisable(true);
 
-  setDisable(true);
-
-  const requestData = {
-    ...data,
-    userType: "EMPLOYEE",
-    tenantId: data.city.code,
+    const requestData = {
+      ...data,
+      userType: "EMPLOYEE",
+    };
+    requestData.tenantId = data.city.code;
+    delete requestData.city;
+    try {
+      const { UserRequest: info, ...tokens } = await Digit.UserService.authenticate(requestData);
+      Digit.SessionStorage.set("Employee.tenantId", info?.tenantId);
+      setUser({ info, ...tokens });
+    } catch (err) {
+      setShowToast(err?.response?.data?.error_description || "Invalid login credentials!");
+      setTimeout(closeToast, 5000);
+    }
+    setDisable(false);
   };
-  delete requestData.city;
-
-  try {
-    const { UserRequest: info, ...tokens } = await Digit.UserService.authenticate(requestData);
-    Digit.SessionStorage.set("Employee.tenantId", info?.tenantId);
-    setUser({ info, ...tokens });
-
-  } catch (err) {
-    setShowToast(err?.response?.data?.error_description || "Invalid login credentials!");
-    setTimeout(closeToast, 5000);
-  }
-
-  setDisable(false);
-};
 
 
 
