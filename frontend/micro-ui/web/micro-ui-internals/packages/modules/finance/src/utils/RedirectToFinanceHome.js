df5f35df93f037d630d1b7585286dfbbd8adf361
umesh-sspl <umesh@sparrowsoftech.net>
2025-05-14 18:19:32 +0530

    finance card updated

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/finance/src/utils/RedirectToFinanceHome.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/finance/src/utils/RedirectToFinanceHome.js
@@ -1,25 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Loader } from "@nudmcdgnpm/digit-ui-react-components";
-import { useRouteMatch } from "react-router-dom/cjs/react-router-dom.min";
+import { useRouteMatch } from "react-router-dom";
 
 const RedirectToFinanceHome = () => {
-  const [loading, setLoading] = useState(true);
-  const { path, url } = useRouteMatch();
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      const baseUrl = process.env.REACT_APP_PROXY_API;
-      const redirectPath = "/employee/services/EGF/voucher/journalVoucher-newForm.action";
-      if (baseUrl && typeof window !== "undefined" && window?.location) {
-        window.location.href = `${baseUrl}${redirectPath}`;
-      }
-    }, 200);
 
-    return () => clearTimeout(timeoutId);
-  }, []);
+  const baseUrl = process.env.REACT_APP_PROXY_API;
+  const redirectPath = "/employee/services/EGF/voucher/journalVoucher-newForm.action";
+
+  if (baseUrl && typeof window !== "undefined" && window?.location) {
+    window.location.href = `${baseUrl}${redirectPath}`;
+  }
 
   return (
     <div className="loader-container">
-      {loading && <Loader />}
+      <Loader />
     </div>
   );
 };
