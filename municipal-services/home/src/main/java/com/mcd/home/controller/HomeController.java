aa66a5d5ef13446f6749d51996e7d121698bfe40
Dr. Bimal Kumar <bimal.kumar@sparrowsoftech.net>
2025-05-26 22:42:30 +0530

    servlet context added

--- municipal-services/home/src/main/java/com/mcd/home/controller/HomeController.java
+++ municipal-services/home/src/main/java/com/mcd/home/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/v1/index")
     public String index() {
         return "index";
     }
